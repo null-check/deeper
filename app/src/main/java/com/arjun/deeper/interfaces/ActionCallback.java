b13ac43472c0efb1a7281998a30bc956d450fdba
null-check <arjun.ajith.dev@gmail.com>
2019-03-06 12:56:27 +0530

    Gradle upgrade

---
+++ app/src/main/java/com/arjun/deeper/interfaces/ActionCallback.java
@@ -0,0 +1,4 @@
+package com.arjun.deeper.interfaces;
+
+public interface ActionCallback {
+}
---
+++ app/src/main/java/com/arjun/deeper/views/gameover/DialogGameOver.java
@@ -0,0 +1,6 @@
+package com.arjun.deeper.views.gameover;
+
+import android.app.Dialog;
+
+public class DialogGameOver extends Dialog {
+}
